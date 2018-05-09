b3985f77281c480ccd4cb862770afbc6628fb47b
root <root@GARVESXPS.localdomain>
2018-05-09 14:27:08 -0400

    aws php dynamo set up

--- mainDBsearch.php
+++ mainDBsearch.php
@@ -1,5 +1,11 @@
 <?php
+session_start();
+require 'aws/aws-autoloader.php';
 require_once("support.php");
+
+use Aws\DynamoDb\Exception\DynamoDbException;
+use Aws\DynamoDb\Marshaler;
+
 $title="Search";
 $form = <<<EOBODY
     <form action="{$_SERVER['PHP_SELF']}" method="post">
@@ -16,7 +22,43 @@ $form = <<<EOBODY
 EOBODY;
 
 
-$alert = "";
-$body = $form.$alert;
+$error = "";
+
+if(isset($_POST['submit'])) {
+    $sdk = new Aws\Sdk([
+        'endpoint'   => 'http://localhost:8000',
+        'region'   => 'us-west-2',
+        'version'  => 'latest'
+    ]);
+
+    $dynamodb = $sdk->createDynamoDb();
+
+    $marshaler = new Marshaler();
+
+    $table = "OMR_Main";
+    $material = $_POST['material'];
+    $tool = $_POST['tool'];
+
+    $item = $marshaler->marshalJson('
+        {
+            "material": ' . $material . ',
+            "tool": ' . $tool . '
+        }
+    ');
+
+    $params = [
+        'TableName' => $table,
+        'Item' => $item
+    ];
+
+    try{
+       $result = $dynamodb->getItem($params);
+       $_SESSION['result'] = $result["Item"];
+       header("Location: results.php");
+    } catch(DynamoDbException $e) {
+        $error = "<h2>There is no data associated with Material: $material and Tool: $tool </h2>";
+    }
+}
+$body = $form.$error;
 $page = generatePage($body, $title);
 echo $page;
